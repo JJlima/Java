f9e58c5dbf5d18c76671321e71e7e5d49feceea7
JJlima <lima.jefferson06@gmail.com>
2016-05-03 20:40:16 -0300

    javaAlterado

--- src/pacote1/cinco.java
+++ src/pacote1/cinco.java
@@ -1,5 +1,13 @@
 package pacote1;
 
+import java.util.Scanner;
+
 public class cinco {
 
+	Scanner t = new Scanner (System.in);
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
 }
--- src/pacote1/dois.java
+++ src/pacote1/dois.java
@@ -2,4 +2,8 @@ package pacote1;
 
 public class dois {
 
+	
+	double m2;
+	
+	System.out.println(); 
 }
